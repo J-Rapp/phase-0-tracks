e28d3ee9036ec900eeafbaee46af7f91f56c84db
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-03 14:21:19 -0700

    Add user input questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "Thank you for applying to Werewolf, Inc."
+puts "What is your name?"
+    name = gets.chomp
+puts "How old are you?"
+    age = gets.chomp
+puts "What year were you born?"
+	year = gets.chomp
+puts "Do you like garlic?"
+	garlic = gets.chomp
+puts "Will you be enrolling in health insurance?"
+	insurance = gets.chomp
+
+p name, age, year, garlic, insurance
