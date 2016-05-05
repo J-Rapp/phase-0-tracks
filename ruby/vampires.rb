938ae676eb247b76780061e6bed59f21fab56d48
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-05 14:50:27 -0700

    Add method to convert y/n string inputs into Boolean values.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,4 @@
-#Release 1 just requires you to gather basic info about the "applicant"
+#Release 1 just requires you to gather basic info about the "applicant".
 
 puts "Thank you for applying to Werewolf, Inc."
 puts "What is your name?"
@@ -8,34 +8,53 @@ puts "How old are you?"
 puts "What year were you born? (yyyy)"
 	year = gets.chomp.to_i
 puts "Do you like garlic? (y/n)"
-	garlic = gets.chomp
+	garlic = gets.chomp[0].downcase
+# The [0] index and .downcase will auto convert any input to a lowercase of the first letter.
 puts "Will you be enrolling in health insurance? (y/n)"
-	insurance = gets.chomp
+	insurance = gets.chomp[0].downcase
 
 # Release 2 requires some programming skillz to add some logic to all this.
 # We're going to be calling on the && and || operators later on, which
 # means we need all our data to be in Boolean (or at least truthy/falsey).
 
-# First, let's make sure their age matches their birth year, and convert that to Boolean.
+# First, let's make a method to check that their age matches their birth
+# year, and then convert that to a Boolean value.
+
 def age_test(y, a)
 	current_year = Time.new.year
 	if current_year - y == a
-		return "true"
+		return true
 	# Let's also consider they might not have had their birthday yet this year.
-	elsif current_year - (y + 1) == a
-		return "true"
+	# Is there a way to condense this with the "if" above? A built in method perhaps?
+	elsif current_year - y == (a + 1)
+		return true
 	else
-		return "false"
+		return false
 	end
 end
 
 correct_age = age_test(year, age)
+
 # We now have a Boolean to see if they are telling the truth about their age.
 
+# Next, let's make a method to convert garlic and insurance preferences into Booleans.
 
-# Next, let's convert garlic and insurance preferences into Booleans.
+def yn_to_boolean(input)
+	if input == "y"
+		return true
+	elsif input == "n"
+		return false
+	else
+		# Let's catch any other answer like "maybe" and not take our chances with vampires
+		return false
+	end
+end
 
-# Now it's time for the logic bits
+garlic_b = yn_to_boolean(garlic)
+insurance_b = yn_to_boolean(insurance)
+
+# Now that we have Boolean values for our input, it's time for the logic bits!
+
 # If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
 # If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
 # If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
@@ -43,4 +62,4 @@ correct_age = age_test(year, age)
 # Otherwise, print “Results inconclusive.”
 
 
-
+# On to Release 3!
