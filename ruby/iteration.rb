c21a5ea647dc553c2899b60b747a2a726f6b5fb7
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-10 10:13:33 -0700

    Complete Release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,8 +1,37 @@
-def simple_method
-	a = "puppies"
-	puts "We're in the method."
-	yield (a)
-	puts "We're leaving the method."
+# def simple_method
+# 	a = "puppies"
+# 	puts "We're in the method."
+# 	yield (a)
+# 	puts "We're leaving the method."
+# end
+
+# simple_method { |yield_parameter| puts "Inside block with parameter #{yield_parameter}"}
+
+
+famous_spots = ["Space Needle", "Fremont Troll", "Underground Tour"]
+
+p famous_spots
+
+famous_spots.each { |spot| puts spot }
+
+p famous_spots
+
+famous_spots.map! do |spot|
+	spot + "z"
 end
 
-simple_method { |yield_parameter| puts "Inside block with parameter #{yield_parameter}"}
+p famous_spots
+
+vegetable_season = {
+	squash: "winter",
+	tomato: "spring",
+	beans: 3,
+	cilantro: "4"
+}
+
+p vegetable_season
+
+vegetable_season.each do |vegetable, season|
+	puts "#{vegetable} is in #{season}"
+end
+
