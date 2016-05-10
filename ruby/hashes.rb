9c2886ceb34bc3f80a51038c44835b06831ee12f
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-09 20:52:22 -0700

    Answers for release 2, not required.

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+# Release 2 questions (for my own reference/sake)
+
+# 1) How do you delete a key-pair value? 
+# hash_name.delete(key)
+
+# 2) What happens if you try to access the value of a key that is not in the hash?
+# It returns a value of 'nil'
+
+# 3) Regarding the answer you got for the previous question, is there a way to set a different default return value when a key is not present in the hash?
+# hash_name.default = "default_value"
+
+# 4) How can you access just the keys of a hash? What data type does this method return?
+# 
+
+# 5) How do you remove everything from the hash at once (returning an empty hash)?
+# 
+
+# 6) How can you access just the values of a hash?
+# 
