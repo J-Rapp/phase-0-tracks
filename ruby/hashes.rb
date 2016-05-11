5e273eb7541f004388a220024a07f5a23594985c
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-10 17:31:58 -0700

    Forgot to commit before closing laptop so can't remember changes, I think it's just pseudocode and a couple basic commands.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -18,5 +18,37 @@
 # 6) How can you access just the values of a hash?
 # hash_name.values
 
+
+
 # Release 3
 
+# Goal of this is to create a Ruby program that collects input from the user (in this case an interior designer) and organizes that data into a hash.
+# They need to be able to enter the following details about their client:
+# Name, Age, Number of children, decor theme, and anything other inputs that will give me a good combo of string, integer, and Boolean values.
+# Keys should be symbols, unless a string is needed for some reason
+
+# First, we need to prompt the user for data input.
+
+puts "What is the client's name?"
+
+puts "What is the client's age?"
+
+puts "How many children does the client have?"
+
+puts "What is the client's decore theme?"
+
+puts "Does the client like crazy patterns?"
+
+puts "Does the client like minimalism"
+
+# Next, we need to convert the input into useable data.
+
+
+
+# Print the hash back out upon completing the input program.
+
+puts client_profile
+
+# Give the opportunity to update the value of a key if needed, or 'none' if no changes are needed. For the purpose of the assignment, we can assume that the user will input a correct/pre-existing key when prompted - no need to check for errors.
+
+# Print the updated version of the hash and exit the program.
