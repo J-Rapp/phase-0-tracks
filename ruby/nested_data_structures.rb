e8b384c1f947b16154e4227212aefcc0f7a27e33
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-12 21:09:56 -0700

    Correct all syntax errors for nested data.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,36 +1,39 @@
 # Nested data structure of a multi-floor apartment complex
 
 apartment = {
-	floor_1 = {
-		units: 10
-		residents: 11
+	floor_1: {
+		units: 10,
+		residents: 11,
 		unit_types: [
-			"Studio"
+			"Studio",
 			"One Bedroom, One Bath"
-		]
-		filled => true
-	}
-	floor_2 = {
-		units: 8
-		residents: 16
+		],
+		filled: true
+	},
+	floor_2: {
+		units: 8,
+		residents: 16,
 		unit_types: [
 			"One Bedroom, One Bath",
 			"Two Bedroom, One Bath",
 			"Two Bedroom, Two Bath"
-		]
-		filled => false
-	}
-	floor_3 = {
-		units: 4
-		residents: 6
+		],
+		filled: false
+	},
+	floor_3: {
+		units: 4,
+		residents: 6,
 		unit_types: [
 			"Executive Loft"
-		]
-		filled => true
-	}
-	active_issues = [
+		],
+		filled: true
+	},
+	active_issues: [
 		"Faucet leak in unit 206",
 		"Clogged toilet in unit 301",
 		"Lightbulb out in unit 110"
 	]
 }
+
+# Is the units on second floor filled?
+p apartment[floor_2][filled]
