cc8ed75f2ef3384565533353c4ca3ef1c39caed4
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-13 16:18:26 -0700

    Basic pseudocode laid out

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# I'm coding a program that takes a spy's real name and converts it into an alias
+# I am supposed to meet three criteria:
+	# 1) The first and last names must be switched
+	# 2) Every consonant must index +1
+	# 3) Every vowel must index +1
+
+# I need to break the "name" string into an array of individual letters
+# Should I ask for First/Last seperately, or figure out a way to break into two arrays at the space?
+
+# Next I need to see if there's methods to differentiate between vowels and consonants
+# If there is I can just run that on the array
+# Otherwise I need to figureout a flow that serparates the two
+
+# I need each vowel and consonant to index +1, probably just within a block on the .next method
+
+# Last step is some reassembly of the array into a string with the .join method
