ea4bd5e8d1b53bd5dd6a28c7dbd62e83f8975a55
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-16 12:04:45 -0700

    Complete 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Puppy Class (Species)
+
+# Characteristics
+	# Size: varies
+	# Fur Color: varies
+	# Eyes: 2
+	# Paws: 4
+	# Adorable: yes
+	# Energetic: yes
+
+# Behavior
+	# Run
+	# Chew
+	# Poop
