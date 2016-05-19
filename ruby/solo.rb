ee63f06d5da2c3f8d8897d0f6d99d8cbb94ab468
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-19 16:44:38 -0700

    Complete Release 0, pseudocode class

---
+++ ruby/solo.rb
@@ -0,0 +1,7 @@
+# Release 0 -
+
+# I will create a class for rubber ducks
+# Each duck will vary by color, age (in years), and whether or not it still floats
+# Each duck should be able to quack, bob in the water when disturbed, and stare into your soul
+
+# Release 1 -
