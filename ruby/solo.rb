c58bce3057b74c01046223ea2a0f3fe41baa45a1
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-19 16:59:50 -0700

    Expound pseudocode, create basic Class syntax

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -2,6 +2,32 @@
 
 # I will create a class for rubber ducks
 # Each duck will vary by color, age (in years), and whether or not it still floats
-# Each duck should be able to quack, bob in the water when disturbed, and stare into your soul
+# Each duck should be able to:
+	# quack when squeezed, results depending on "firmness" of squeeze
+	# bob in the water when splashed, results depending on severity of splash
+	# stare into your soul. all the time.
 
 # Release 1 -
+
+class Rubber_duck
+	def initialize(age, color, bouyancy)
+		@age = age
+		@color = color
+		@bouyancy = bouyant
+	end
+	def squeeze(firmness)
+		case firmness
+		when "soft"
+			puts "quack."
+		when "medium"
+			puts "QUACK."
+		when "hard"
+			puts "QUUUUAAAAACK!!!"
+	end
+	def splash(waves)
+		waves.times {|each| puts "bob. "}
+	end
+	def gaze
+		puts "The rubber duck stares into your soul with a peircing, cold gaze."
+	end
+end
