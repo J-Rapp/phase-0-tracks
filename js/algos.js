59aeb84193d59195a872ee0e91e12a92e6b4a4d1
Jacob Rapp <jake.rapp322@gmail.com>
2016-05-27 13:07:35 -0700

    Basic match function working. Might need refactoring.

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,7 @@
+// FUNCTION CODE:
+
+// Release 0
+
 // Create a function that returns the longest string in an array
 // input: an array
 // ideas for steps (in no specific order): 
@@ -8,7 +12,6 @@
 // 		if it's bigger, overwrites that index
 //      optional step - if there is a tie, print both values
 // output: an array of string(s)
-
 
 function longestString(array){
 	var longestStringLength = 0;
@@ -25,20 +28,69 @@ function longestString(array){
 	return array[longestStringIndex]; // or longestStringsArray if two objects are equal length
 }
 
+// Release 1
+
 // create a function that checks if two objects share a key/value pair
 // input: two objects
-// steps: 
-//		
+// ideas for steps (in no specific order): 
+//		using the variable index of an object
+//		using that index, oop through the other object to check for matches
+//		break the index(es) up for character comparison?
+//		use && or || to see if characters match
 // output: true or false
 
+function objectMatcher(object1, object2){
+	var match = false;
+	for (var property in object1) {
+		if (object2.hasOwnProperty(property)) {
+			match = (object1[property]==object2[property]);
+				if (match) {
+					return match;
+				}
+		}
+	}
+	return match
+}
 
 
 
+// DRIVER CODE
+
+// // Release 0
+
+// var testArray1 = ["tubas", "bears", "corvettes", "hyenas"];
+// console.log(longestString(testArray1))
+// var testArray2 = ["a", "did you know?", "well hey there", "surprise"]
+// console.log(longestString(testArray2))
+
+// Release 1
+
+var baby = {
+	age: 1,
+	basicallyDrunk: "yes",
+	cirrhosis: false,
+	"dumb": true
+}
+var drunkPerson = {
+	age: 56,
+	basicallyDrunk: "yes",
+	cirrhosis: true,
+	dumb: true
+}
+
+console.log(objectMatcher(baby, drunkPerson));
+
+var animal = {
+	mineral: false,
+	distinct: "true" // when referencing Object[property], data type apparently matters.
+}
+
+var mineral = {
+	animal: false,
+	distinct: true
+}
+
+console.log(objectMatcher(animal, mineral));
 
 
 
-
-var testArray1 = ["tubas", "bears", "corvettes", "hyenas"];
-console.log(longestString(testArray1))
-var testArray2 = ["a", "did you know?", "well hey there", "surprise"]
-console.log(longestString(testArray2))
