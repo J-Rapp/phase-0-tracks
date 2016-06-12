b5e376a73f0864ed4c1993eac10ed26fc8763d55
Jacob Rapp <jake.rapp322@gmail.com>
2016-06-12 16:59:22 -0700

    Finish Release 0

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -15,8 +15,8 @@ end
 # write a GET route with
 # route parameters
 get '/about/:person' do
-  person = params[:person]
-  "#{person} is a programmer, and #{person} is learning Sinatra."
+  # person = params[:person]
+  "#{params[:person]} is a programmer, and #{params[:person]} is learning Sinatra."
 end
 
 get '/:person_1/loves/:person_2' do
@@ -43,4 +43,44 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+# release 0
+# write a GET route that retrieves
+# an address
+
+get "/address" do
+  "The student's address is #{params[:address]}"
+end
+
+# write a GET route that
+# takes a name as a query parameter or else returns a generic phrase
+
+get "/great_job" do
+  name = params[:name]
+  if name
+    "Great job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# write a GET route that
+# adds two parameters together
+
+get "/add/:num1/:num2" do
+  sum = params[:num1].to_i + params[:num2].to_i
+  sum.to_s
+end
+
+# write a GET route that
+# searches the database for a certain criteria
+
+get "/location/:thing" do
+  list = db.execute("SELECT name FROM students WHERE campus=?", [params[:thing]])
+  result = ""
+  list.each do |person|
+    result << "#{person["name"]}<br>"
+  end
+  result
 end
